08e3d73a6eadd186a007f0e3fd3bab259f9e5cc1
Kunihisa Matsuda <love_coke@mac.com>
2026-02-25 14:02:24 +0900

    fix page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
+    import '$lib/assets/layout.css';
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
 </svelte:head>
 
+
+  <div class="container">
+    <div class="content">
 {@render children()}
+    </div>
+  </div>
